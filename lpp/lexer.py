c3d54ba5aef350b52627989be5f0d4907dbefd5f
Hector Franco T <tor6.franco6@hotmail.com>
2021-07-26 08:49:07 -0500

    Add read numbers and words literals

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -1,4 +1,4 @@
-from lpp.token import Token, TokenType
+from lpp.token import Token, TokenType, lookup_token_type
 from re import compile
 
 TOKEN_REGEX = [
@@ -12,6 +12,9 @@ TOKEN_REGEX = [
     (compile(r'^,$'), TokenType.COMMA),
     (compile(r'^;$'), TokenType.SEMICOLON),
 ]
+LETTER_REGEX = compile(r'^[a-záéíóúA-ZÁÉÍÓÚñÑ_]$')
+NUMBER_REGEX = compile(r'^[0-9]$')
+WHITESPACE_REGEX = compile(r'^\s+$')
 
 class Lexer:
     def __init__(self, source: str) -> None:
@@ -23,15 +26,32 @@ class Lexer:
         self._read_character()
 
     def next_token(self) -> Token:
+        self._skip_whitespace()
         for (regex, token_type) in TOKEN_REGEX:
             if regex.match(self._character):
                 token = Token(token_type, self._character)
                 self._read_character()
                 return token
 
+        if self._is_letter(self._character):
+            literal = self._read_identifier()
+            token_type = lookup_token_type(literal)
+
+            return Token(token_type, literal)
+
+        if self._is_number(self._character):
+            literal = self._read_number()
+            return Token(TokenType.INT, literal)
+
         token =  Token(TokenType.ILLEGAL, self._character)
         self._read_character()
         return token
+
+    def _is_letter(self, character: str) -> bool:
+        return bool(LETTER_REGEX.match(character))
+
+    def _is_number(self, character: str) -> bool:
+        return bool(NUMBER_REGEX.match(character))
 
     def _read_character(self) -> None:
         if self._read_position >= len(self._source):
@@ -40,3 +60,23 @@ class Lexer:
             self._character = self._source[self._read_position]
             self._position = self._read_position
             self._read_position += 1
+
+    def _read_identifier(self) -> str:
+        initial_position = self._position
+
+        while self._is_letter(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+
+    def _read_number(self) -> str:
+        initial_position = self._position
+
+        while self._is_number(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+
+    def _skip_whitespace(self) -> None:
+        while WHITESPACE_REGEX.match(self._character):
+            self._read_character()
