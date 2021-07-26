c59398171362d89615a4fb454fb208eba70000d1
Hector Franco T <tor6.franco6@hotmail.com>
2021-07-26 16:33:13 -0500

    Add REPL initial version

---
+++ main.py
@@ -0,0 +1,10 @@
+from lpp.repl import start_repl
+
+def main() -> None:
+    print('--- Bienvenido ---')
+    print('Escribe un comando\n')
+
+    start_repl()
+
+if __name__ == "__main__":
+    main()
