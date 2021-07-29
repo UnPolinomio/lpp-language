44635de3834b42303046edfc9afcec3600f91691
Hector Franco T <tor6.franco6@hotmail.com>
2021-07-29 14:15:17 -0500

    Update repl to show parsed program string

--- lpp/repl.py
+++ lpp/repl.py
@@ -1,4 +1,6 @@
+from lpp.ast import Program
 from lpp.lexer import Lexer
+from lpp.parser import Parser
 from lpp.token import (
     Token,
     TokenType,
@@ -6,10 +8,19 @@ from lpp.token import (
 
 EOF_TOKEN = Token(TokenType.EOF, '')
 
+def _print_parse_errors(errors: list[str]) -> None:
+    for error in errors:
+        print('--- Error ---')
+        print(error)
+
 def start_repl() -> None:
     while (source := input('>> ')) != 'salir()':
         lexer = Lexer(source)
+        parser = Parser(lexer)
+        program = parser.parse_program()
 
-        while (token := lexer.next_token()) != EOF_TOKEN:
-            print(token)
-            
+        if len(parser.errors) > 0:
+            _print_parse_errors(parser.errors)
+            continue
+
+        print(program)
